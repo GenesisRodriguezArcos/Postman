687a7d51cb55427e0030a44ff6f39588802e7f97
Genesis Rodriguez <genesis.rodriguez.arcos@vallegrande.edu.pe>
2025-03-28 09:54:29 -0500

    new schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,25 @@ CREATE TABLE customer (
     last_name VARCHAR(50) NOT NULL,
     state CHAR(1) NOT NULL
 );
+
+CREATE TABLE seller(
+    id_seller INT PRIMARY KEY,
+    dni CHAR(8) NOT NULL,
+    seller_name VARCHAR(100) NOT NULL,
+    address VARCHAR(150) NOT NULL
+    state CHAR(1) NOT NULL
+);
+
+CREATE TABLE employee(
+    id_employee INT PRIMARY KEY,
+    dni CHAR(8) NOT NULL,
+    employee_name VARCHAR(100) NOT NULL,
+    post VARCHAR(50)
+);
+
+CREATE TABLE buyer(
+    id_buyer INT PRIMARY KEY,
+    dni CHAR(8) NOT NULL,
+    buyer_name VARCHAR (100) NOT NULL,
+    buyer_lastName VARCHAR (100) NOT NULL
+);
